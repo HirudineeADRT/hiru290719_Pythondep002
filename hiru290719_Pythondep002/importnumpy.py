752c31670dfcfc271d54a89b5cb1ce1c09d495aa
HirudineeADRT <hirudinee@adroitlogic.com>
2019-07-29 15:16:16 +0530

    added requests

--- hiru290719_Pythondep002/importnumpy.py
+++ hiru290719_Pythondep002/importnumpy.py
@@ -1,5 +1,9 @@
 import numpy
+import requests
 
 def handler(event, context):
+
     x = numpy.linspace(0, 10, 5000)
+    r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+    print (r)
     return {"message": str(x)}
